c13c59d748f81793fb365bdfe695769a53798b6c
Stephanie Marin <stephanienia05@gmail.com>
2023-05-10 15:36:32 -0400

    - reverse geocode case study

--- Reverse Geocode Live Demo 01/ViewController.m
+++ Reverse Geocode Live Demo 01/ViewController.m
@@ -6,8 +6,16 @@
 //
 
 #import "ViewController.h"
+@import CoreLocation;
+@import MapKit;
 
 @interface ViewController ()
+//core location geocoder
+
+@property (strong, nonatomic) CLGeocoder *geocoder;
+@property (weak, nonatomic) IBOutlet MKMapView *MapView;
+@property (weak, nonatomic) IBOutlet UILabel *geocodeLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *pinIcon;
 
 @end
 
@@ -15,8 +23,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.geocoder = [[CLGeocoder alloc] init];
+    self.geocodeLabel.text = nil;
+    self.geocodeLabel.alpha = 0.5;
 }
 
+- (IBAction)reverseGeocodeTapped:(id)sender {
+    //create a coodinate based on current map orientation
+    CLLocationCoordinate2D coord = [self locationAtCenterOfMapView];
+    CLLocation *loc = [[CLLocation alloc] initWithLatitude:coord.latitude longitude:coord.longitude];
+    [self startReverseGeocodeLocation: loc];
+}
 
+- (CLLocationCoordinate2D)locationAtCenterOfMapView {
+    CGPoint centerOfPin = CGPointMake(CGRectGetMidX(self.pinIcon.bounds), CGRectGetMidY(self.pinIcon.bounds)); // in ImageView coord space
+    return [self.MapView convertPoint:centerOfPin toCoordinateFromView:self.pinIcon];  // getting the location at center of pin
+}
+
+- (void)startReverseGeocodeLocation : (CLLocation *)location //getting the request from the cloud
+{
+    [self.geocoder reverseGeocodeLocation:location completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        if (error) {
+            UIAlertController *ac = [UIAlertController alertControllerWithTitle:@"There was a problem reverse geocoding" message:[error localizedDescription] preferredStyle:UIAlertControllerStyleAlert];
+            [self presentViewController:ac animated: YES completion:nil];
+            return;
+        }
+        //Grab some interesting bits of CLPlacemark and show it.
+        // but no dupes.
+        
+        NSMutableSet *mappedPlaceDescs = [NSMutableSet new];
+        for (CLPlacemark *p in placemarks) {
+            if (p.name != nil)
+                [mappedPlaceDescs addObject:p.name];
+            if (p.administrativeArea != nil)
+                [mappedPlaceDescs addObject:p.administrativeArea];
+            if (p.country != nil)
+                [mappedPlaceDescs addObject:p.country];
+        }
+        self.geocodeLabel.text = [[mappedPlaceDescs allObjects]
+            componentsJoinedByString:@"\n"];
+        self.geocodeLabel.alpha = 1.0;
+        
+    }];
+}
 @end
